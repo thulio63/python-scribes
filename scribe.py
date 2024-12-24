48ea411ffdafe91f9d2ddd2c9c19870b473b9275
Andrew Thul <thulio63@gmail.com>
2024-12-24 11:34:27 -0600

    Midday update: further features made interactive

--- scribe.py
+++ scribe.py
@@ -51,34 +51,35 @@ class Canvas():
             #add [] inside print() for visible border
         
 class Format:
+    """Allows for different text formatting effects."""
     end = '\033[0m'
     underline = '\033[4m'
-
-endingPos = [0,0]
-
-def _logEndPos(pos: list): #makes scribes follow from previous scribes
-    global endingPos 
-    endingPos = pos
-    
+  
 class TerminalScribe:
 
     fresh = True
     scribesList = []
     shapes = "line", "square", "function", "ln", "sq", "func"
+    endingPos = [0,0]
 
     def __init__(self, canvas: Canvas):
         self.canvas = canvas
-        #self.pos = [0, 0] #start in top left
-        self.pos = [(self.canvas._x / 2), self.canvas._y / 2]
+        self.pos = [0, 0] #start in top left
+        #self.pos = [(self.canvas._x / 2), self.canvas._y / 2] #start in center
         
         self.framerate = 0.05
         self.direction = [0.0, 0.0]
         self.willBonk = True
+        self.type = ""
         #myCanvas.clear()
 
         self.name = ""
         self.mark = '*'
         self.trail = '.'      
+
+    def _logEndPos(pos: list): #makes scribes follow from previous scribes
+        global endingPos 
+        endingPos = pos
 
     def _bonk(self, pos: list):
         """Makes the scribe bounce off of walls.""" 
@@ -111,70 +112,145 @@ class TerminalScribe:
         self.canvas.setPos(self.pos, self.trail)
         self.canvas.print()
 
-    def createScribe(name: str, shape: str, size: int = 5, angle: int = 90):
-        """Adding a scribe to a list of dictionaries."""
-        newScribe = {"Name":name, "Size":size}
-        if angle == 0:
-            angle = 360
-        match shape.lower(): #determines what shape to store in dict
-            case "line":
-                func = "line"
-            case "square":
-                func = "square"
-                #checks size to see if it fits the canvas/is an int
-                if type(size) != int or size < 1 or size > myCanvas._x:
-                    while True:
-                        try:
-                            newSize = int(input(f"{name}'s size is invalid. Please provide a valid size:\n"))
-                        except ValueError:
-                            continue
-                        if newSize < 1 or newSize > myCanvas._x:
-                            continue
-                        else:
-                            newScribe["Size"] = newSize
-                            break
-            case "func" | "function":
-                formList = ["sin", "sine", "cos", "cosine", "tan", "tangent"]
-                func = "formula" 
-                #asks for ident to use
-                formula = input(f"Which of the trigonometric identities should {name} graph?\n").lower()
-                #ask for what to take func of in future? assign input to some x variable
-                if formula.lower() not in formList:
-                    while True:
-                        try:
-                            newFormula = input(f"That is not a valid identity. Please enter a valid identity:\n").lower()
-                        except Exception:
-                            continue
-                        if newFormula not in formList:
-                            continue
-                        else: 
-                            formula = newFormula
-                            break
-                if formula == "sin" or formula == "sine":
-                    newScribe["Calc"] = "sin" #will make sin graph
-                elif formula == "cos" or formula == "cosine":
-                    newScribe["Calc"] = "cos" #will make cos graph
-                elif formula == "tan" or formula == "tangent":
-                    newScribe["Calc"] = "tan" #will make tan graph               
-            case _:
-                #asks for apology if shape is invalid
-                while True:
-                    data = input(f"{name} will not draw this filth! Apologize!\n")                    
-                    if data.lower() != "sorry" or "sry":
-                        continue
-                    else: #kills self if you apologize, lol
-                        os._exit(1) #figure this out later, maybe just a vscode thing
+    def createScribe():
+        """Adding a scribe to a master list."""
+        if len(TerminalScribe.scribesList) == 0:
+            scribeName = input("\nWhat would you like to name your first scribe?\n")
+        else: scribeName = input("\nWhat would you like to name this scribe?\n")
+        naming = True
+        while naming:
+            if len(TerminalScribe.scribesList) == 0:
+                naming = False
+            for i in TerminalScribe.scribesList:
+                #add .lower() after both to ignore cases in name matching
+                if i.name == scribeName:
+                    #if this name has already been added
+                    scribeName = input("\nThere is already a scribe with this name! Choose another:\n")
+                    continue
+                else: 
+                    naming = False
 
-        newScribe["Function"] = func
+        scribeType = input(f"\nWhat would you like {scribeName} to draw? \nFor now, choose between a line, a square, or a function.\n").lower()
+        shaping = True
+        while shaping:
+            if scribeType not in TerminalScribe.shapes:
+                scribeType = input("\nThat is not a valid shape; please choose one of the listed options.\n")
+                continue
+    
+            else: 
+                shaping = False
+
+        match scribeType:
+            case "line" | "ln":
+                scribeType = "line"
+                #ensure length is an int above 0
+                while True:
+                    try:
+                        lineLen = int(input(f"\nHow long should {scribeName} be?\t"))
+                    except Exception:
+                        print("\nThat is not a valid length; please ensure your answer is an int.")
+                        continue
+                    
+                    if lineLen < 1:
+                        print("Your answer cannot be smaller than 1.\n")
+                        continue
+                    else: break
+                #dictates angle of line
+                while True:
+                    try:
+                        lineAng = int(input(f"What angle (in degrees) should {scribeName} be?\t"))
+                    except Exception:
+                        print("\nThat is not a valid angle; please ensure your answer is an int.\n")
+                        continue
+                    break
+                
+                #scribe is created and added to master list
+                scribe = LineScribe(scribeName, lineLen, lineAng)
+                TerminalScribe.scribesList.append(scribe)
+
+                buildMore = input(f"\nYou have created a scribe named {scribeName} that will draw a {lineAng} degree {scribeType}. Would you like to make more scribes?\n\t").lower()
+
+            case "square" | "sq":
+                scribeType = "square"
+                #ensure size is an int above 0 and below canvas boundaries
+                while True:
+                    try:
+                        lineLen = int(input(f"\nHow long should {scribeName}'s sides be?\t"))
+                    except Exception:
+                        print("\nThat is not a valid length; please ensure your answer is an int.")
+                        continue
+                    
+                    if lineLen < 1:
+                        print("Your answer cannot be smaller than 1.\n")
+                        continue
+                    if lineLen > myCanvas._x or lineLen > myCanvas._y:
+                        print(f"Your answer cannot be larger than the canvas, which is {myCanvas._x} units wide and {myCanvas._y} units tall.\n")
+                        continue
+
+                    else: break
+                #checks if square should be centered
+                isCentered = input(f"Should {scribeName} be centered on the canvas?\t").lower()
+                ansCent = True
+                while ansCent:
+                    match isCentered:
+                        case "y" | "yes":
+                            ansCent = False
+                            _centered = True
+                            break
+                        case "n" | "no":
+                            ansCent = False
+                            _centered = False
+                            break
+                        case _:
+                            isCentered = input("Please answer yes or no:\n\t").lower()
+                            continue
+                #scribe is created and added to master list
+                scribe = SquareScribe(scribeName, lineLen, _centered)
+                TerminalScribe.scribesList.append(scribe)
+
+                #perhaps indicate if square is centered?
+                buildMore = input(f"\nYou have created a scribe named {scribeName} that will draw a {scribeType} with {lineLen} unit long sides. Would you like to make more scribes?\n\t").lower()
+
+            case "function" | "func":
+                pass
+            case _:
+                pass
+            #fill out with drawing different shapes
+
+        #asks to continue, catches anything but yes or no
+        ansBuild = True
+        while ansBuild:
+            match buildMore:
+                case "y" | "yes":
+                    ansBuild = False
+                    TerminalScribe.createScribe()
+                    break
+                case "n" | "no":
+                    ansBuild = False
+                    break
+                case _:
+                    buildMore = input("Please answer yes or no:\n\t").lower()
+                    continue
+
+    def sendScribe():
+        drawing = True
+        commandName = input("\nWhich scribe would you like to summon?\t")
+        while drawing: 
+            for i in TerminalScribe.scribesList:
+                if i.name == commandName:
+                    if i.type == "line":
+                        i.draw(i.length, i.angle, i.pos)
+                        drawing = False       
+                    elif i.type == "square":
+                        i.draw(i.size, i.centered)
+                        drawing = False 
+                    elif i.type == "function":
+                        i.draw(i.size, i.centered)
+                        drawing = False 
+            if drawing: 
+                commandName = input("\nThere is no scribe with such a name. Please name an existing scribe:\t")
+                continue
         
-        if angle:
-            newScribe["Angle"] = angle
-        for i in TerminalScribe.scribesList:
-            if newScribe["Name"] == i["Name"]:
-                #maybe undo this to allow for auto-naming?
-                return print("There is already a scribe with this name!") 
-        
-        TerminalScribe.scribesList.append(newScribe)
 
     def summonScribe(name: str):
         """Tells specified scribe to make its shape."""
@@ -208,7 +284,8 @@ class LineScribe(TerminalScribe):
         self.name = name
         self.length = length
         self.angle = angle
-        self.currentPos = currentPos
+        self.pos = currentPos
+        self.type = "line"
         
 
     def draw(self, size: int, angle: int, currentPos):        
@@ -218,10 +295,17 @@ class LineScribe(TerminalScribe):
         self._face(angle)
         self._forward(size)
 
-        _logEndPos(self.pos)
+        TerminalScribe._logEndPos(self.pos)
         self._clearMark()    
 
 class SquareScribe(TerminalScribe):
+    def __init__(self, name: str, size: int, centered: bool = True, currentPos: list = [0,0]):
+        super().__init__(myCanvas)
+        self.name = name
+        self.size = size
+        self.centered = centered
+        self.pos = currentPos
+        self.type = "square"
 
     def _right(self, spaces):
         self._face(90) 
@@ -239,12 +323,14 @@ class SquareScribe(TerminalScribe):
         self._face = 360
         self._forward(spaces)
     
-    def draw(self, size: int, currentPos: list= [0,0]):
+    def draw(self, size: int, centered: bool):
         #for rectangles (self, width: int, height: int) swap "size" with width and height
-        if currentPos == [0, 0]:
+
+        if centered:
+            self.centered = True
             self.pos = [(int((self.canvas._x - size) / 2)), (int((self.canvas._y - size) / 2))]
         else:
-            self.pos = currentPos
+            self.pos = TerminalScribe.endingPos
         self.canvas.setPos(self.pos, self.mark) 
 
         self._right(size-1)
@@ -258,13 +344,12 @@ class SquareScribe(TerminalScribe):
         self._face(0)
         self._forward(size-1) #height for rect 
 
-        _logEndPos(self.pos)
+        TerminalScribe._logEndPos(self.pos)
         self._clearMark()
 
     """In interactive mode, user creates scribe. new <name> SquareScribe 
     object is created. On order to run, code <name>.draw(*args)"""
-
-    
+  
 class FunctionScribe(TerminalScribe):
     def draw(self, identity: str):
         self.pos = [0, int(self.canvas._y / 2)]
@@ -312,116 +397,33 @@ class FunctionScribe(TerminalScribe):
             time.sleep(self.framerate)
         self._clearMark()
 
-creating = True
 #begins program with user input
 
 #make canvas first!
-myCanvas = Canvas(50, 50) #make input
+myCanvas = Canvas(20, 20) #make input
 
 #greeting
 print("Welcome! Let's make some scribes together :)")
-while creating:
-    if len(TerminalScribe.scribesList) == 0:
-        scribeName = input("\nWhat would you like to name your first scribe?\n")
-    else: scribeName = input("\nWhat would you like to name this scribe?\n")
-    naming = True
-    while naming:
-        if len(TerminalScribe.scribesList) == 0:
-            naming = False
-        for i in TerminalScribe.scribesList:
-            #add .lower() after both to ignore cases in name matching
-            if i.name == scribeName:
-                #if this name has already been added
-                scribeName = input("\nThere is already a scribe with this name! Choose another:")
-                continue
-            else: 
-                naming = False
 
-    scribeType = input(f"\nWhat would you like {scribeName} to draw? \nFor now, choose between a line, a square, or a function.\n").lower()
-    shaping = True
-    while shaping:
-        if scribeType not in TerminalScribe.shapes:
-            scribeType = input("\nThat is not a valid shape; please choose one of the listed options.\n")
-            continue
- 
-        else: 
-            shaping = False
-
-    match scribeType:
-        case "line" | "ln":
-            scribeType = "line"
-            #ensure length is an int above 0
-            while True:
-                try:
-                    lineLen = int(input(f"\nHow long should {scribeName} be?\t"))
-                except Exception:
-                    print("\nThat is not a valid length; please ensure your answer is an int.")
-                    continue
-                
-                if lineLen < 1:
-                    print("Your answer cannot be smaller than 1.\n")
-                    continue
-                else: break
-            #dictates angle of line
-            while True:
-                try:
-                    lineAng = int(input(f"What angle (in degrees) should {scribeName} be?\t"))
-                except Exception:
-                    print("\nThat is not a valid angle; please ensure your answer is an int.\n")
-                    continue
-                break
-            
-            #scribe is created and added to master list
-            scribe = LineScribe(scribeName, lineLen, lineAng)
-            TerminalScribe.scribesList.append(scribe)
-
-            buildMore = input(f"\nYou have created a scribe named {scribeName} that will draw a {lineAng} degree {scribeType}. Would you like to make more scribes?\n\t").lower()
-            #asks to continue, catches anything but yes or no
-            ansBuild = True
-            while ansBuild:
-                match buildMore:
-                    case "y" | "yes":
-                        ansBuild = False
-                        break
-                    case "n" | "no":
-                        ansBuild = False
-                        creating = False
-                        break
-                    case _:
-                        buildMore = input("Please answer yes or no:\n\t").lower()
-                        continue
-
-        case "square" | "sq":
-            pass
-        case "function" | "func":
-            pass
-        case _:
-            pass
-        #fill out with drawing different shapes
+TerminalScribe.createScribe()
     
-    
+TerminalScribe.sendScribe()
 
-#once all scribes are created, run specified scribe/other functions to be added (list them, run multiple, whatever)
-drawing = True
-commandName = input("\nWhich scribe would you like to summon?\t")
-while drawing: 
-    for i in TerminalScribe.scribesList:
-        if i.name == commandName:
-            i.draw(i.length, i.angle, i.currentPos)
-            drawing = False           
-        else: 
-            commandName = input("\nThere is no scribe with such a name. Please name an existing scribe:\t")
-            continue
 prompt = input(f"Would you like to {Format.underline}summon{Format.end} another scribe, {Format.underline}create{Format.end} a new scribe, or {Format.underline}quit{Format.end}?\n").lower()
 choosing = True
 while choosing:
     match prompt:
         case "summon" | "s":
             choosing = False
-            pass
+            TerminalScribe.sendScribe()
+            print(endingPos)
+            prompt = input(f"Would you like to {Format.underline}summon{Format.end} another scribe, {Format.underline}create{Format.end} a new scribe, or {Format.underline}quit{Format.end}?\n").lower()
+            continue
         case "create" | "c":
             choosing = False
-            pass
+            TerminalScribe.createScribe()
+            prompt = "summon"
+            continue
         case "quit" | "q":
             choosing = False
             print("yayayayAYAYAYAYAY YOU'RE FREE!!!!!")
